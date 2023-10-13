1a4edbbde17b5db1eaa262377f5e978a7866edae
samueloliveira_m <trabalho.samueloliv@gmail.com>
2023-10-13 09:04:29 -0300

    suplier create implementation

--- src/services/supllierServices/SupllierServices.ts
+++ src/services/supllierServices/SupllierServices.ts
@@ -1,15 +1,41 @@
+import {hash} from 'bcryptjs'
+
 import { prisma } from "../../lib/prisma";
 
+interface ISuplierRequest{
+  nomeDaEmpresa: string,
+  endereco: string,
+  contato: string,
+  email: string,
+  senha: string
+}
+
+
 class SupplierService {
-  async createSupplier(nomeDaEmpresa: string, endereco: string, contato: string, email: string) {
-    return prisma.supplier.create({
-      data: {
+  async createSupplier({nomeDaEmpresa,endereco,contato,email,senha}:ISuplierRequest) {
+    
+    const suplierAlreadyExists = await prisma.supplier.findFirst({
+      where:{
+        email
+      } 
+    })
+
+    if(suplierAlreadyExists){ 
+      new Error("Suplier already exists")
+    }
+
+    const passwordHash = await hash(senha,8)
+    
+    const supplier = await prisma.supplier.create({
+      data:{
         nomeDaEmpresa,
         endereco,
         contato,
         email,
-      },
-    });
+        senha:passwordHash,
+      }
+    })
+    return supplier;
   }
   
   async list(){
